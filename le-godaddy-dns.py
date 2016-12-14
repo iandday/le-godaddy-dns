9fe009ee95f494feb4099b7c63724e6dcb776c34
Ian Day <iandday@gmail.com>
2016-12-13 20:50:05 -0500

    setup almost complete

--- le-godaddy-dns.py
+++ le-godaddy-dns.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 CONFIG_SOPHOS = 'sophos'
-CA_TEST = 'REF_Ca'
+CA_GREP = 'REF_Ca'
 
 
 def write_domains_file(domains, out_file):
@@ -10,26 +10,6 @@ def write_domains_file(domains, out_file):
     with open(out_file, 'w') as output:
         for domain in domains:
             output.write('%s\n'%domains[domain])
-
-
-def load_domains_file(config_file):
-    import ConfigParser
-
-    config_parser = ConfigParser.SafeConfigParser()
-    config_parser.read(config_file)
-    domains = {}
-
-    for name, value in config_parser.items(CONFIG_SOPHOS):
-        try:
-            cert_key = value.split('[')[0]
-            if cert_key[:6] == CA_TEST:
-                url = raw_input('Enter URL for certificate %s:' % cert_key)
-                domains[cert_key] = url
-            else:
-                continue
-        except:
-            print("Problem reading config file")
-    return domains
 
 
 def load_domains():
@@ -43,39 +23,59 @@ def load_domains():
         if line:
             for segment in line.split(','):
                 if segment.split() and segment.split("'")[3][0:6] == 'REF_Ca':
-                    domains[segment.split("'")[3]] = raw_input("Full domain for certificate %s: "%segment.split("'")[3])
+                    domains[segment.split("'")[3]] = raw_input("Full domain for certificate %s: "
+                                                               % segment.split("'")[3])
     return domains
 
 
-def setup(root):
-    import urllib
+def setup(root_path):
+    import urllib2
     import zipfile
 
 
     # git isn't installed by default
-    urllib.urlretrieve('http://github.com/lukas2511/dehydrated/archive/master.zip', 'master.zip')
-    zip = zipfile.ZipFile('master.zip')
-    zip.extractall()
-    os.remove('master.zip')
-    urllib.urlretrieve('http://github.com/lrklomp/sophos-utm-letsencrypt/archive/master.zip', 'master.zip')
+    os.chdir(root_path)
+    with open('master.zip', 'wb') as f:
+        f.write(urllib2.urlopen('https://github.com/lukas2511/dehydrated/archive/master.zip').read())
+        f.close()
     zip = zipfile.ZipFile('master.zip')
     zip.extractall()
     os.remove('master.zip')
 
-    # load sophos cc output
+    with open('master.zip', 'wb') as f:
+        f.write(urllib2.urlopen('https://github.com/rklomp/sophos-utm-letsencrypt/archive/master.zip').read())
+        f.close()
+    zip = zipfile.ZipFile('master.zip')
+    zip.extractall()
+    os.remove('master.zip')
+
+    with open('config', 'wb') as f:
+        f.write(urllib2.urlopen('https://raw.githubusercontent.com/iandday/le-godaddy-dns/master/config').read())
+        f.close(
+
+    # query sophos UTM for cert information
     domains = load_domains()
     # create domains file for dehydrated
     write_domains_file(domains, os.path.join('dehydrated-master', 'domains.txt'))
 
+    #write GoDaddy keys file
+    with open('keys', 'w') as output:
+        output.write('[go_daddy]\n')
+        output.write('api_key = ' + raw_input('GoDaddy API Key: ') + '\n')
+        output.write('api_secret = ' + raw_input('GoDaddy API Secret: ') + '\n')
+        output.close()
+    return domains
 
-
-root_dir = os.getcwd()
+root_dir = '/root/letsencrypt'
 certs_path = os.path.join(root_dir, 'dehydrated-master', 'certs')
 daily_command = 'dehydrated-master/dehydrated  -k godaddy.py -c'
 
 
+if not os.path.exists(root_dir):
+    os.makedirs(root_dir)
+domain_dict = setup(root_dir)
 
-#setup()
-load_domains()
+
+
 
 
